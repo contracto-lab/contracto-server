00c0c0d372c9c8fb1ef773815cdf34455572f253
Kacper Walanus <kacper@walanus.com>
2015-03-12 20:04:13 +0100

    run server in background

--- lib/contracto/runner.rb
+++ lib/contracto/runner.rb
@@ -1,11 +1,8 @@
 class Contracto::Runner
   PORT = 54321
 
-  def initialize(args)
-
-  end
-
   def execute
-    system "rackup contracto/config.ru -p #{PORT}"
+    system "rackup contracto/config.ru -p #{PORT} &"
+    system "while ! echo exit | nc localhost #{PORT} > /dev/null && echo \"Waiting for server...\"; do sleep 1; done"
   end
 end
