534586a24cd8a1606ea54fbdfda48d03492ef0cf
Kacper Walanus <kacper@walanus.com>
2015-03-12 22:52:10 +0100

    run server daemonized

--- lib/contracto/runner.rb
+++ lib/contracto/runner.rb
@@ -5,7 +5,8 @@ class Contracto::Runner
   end
 
   def execute
-    system "rackup contracto/config.ru -p #{PORT} &"
+    system "rackup #{Contracto::CONTRACTO_DIR}/config.ru -p #{PORT} -D -P #{Contracto::CONTRACTO_DIR}/server.pid"
     system "while ! echo exit | nc localhost #{PORT} > /dev/null && echo \"Waiting for server...\"; do sleep 1; done"
+    system "curl 0.0.0.0:#{PORT}"
   end
 end
