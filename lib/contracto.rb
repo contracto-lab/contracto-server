05b1494e430683cf8d09d83cbd150519986ea282
Kacper Walanus <kacper@walanus.com>
2015-03-12 23:19:35 +0100

    better formatting

--- lib/contracto.rb
+++ lib/contracto.rb
@@ -1,11 +1,11 @@
 require "contracto/version"
 
 module Contracto
-  GEM_DIR = Gem::Specification.find_by_name('contracto').gem_dir
-  CONTRACTO_DIR = 'contracto'
-  CONTRACTO_TMP_DIR = '.tmp.contracto'
-  RUBY_SERVER_DIR = "#{GEM_DIR}/lib/contracto/server/ruby"
-  CONTRACT_FILENAME = 'contract.cdc.rb'
+  GEM_DIR               = Gem::Specification.find_by_name('contracto').gem_dir
+  CONTRACTO_DIR         = 'contracto'
+  CONTRACTO_TMP_DIR     = '.tmp.contracto'
+  RUBY_SERVER_DIR       = "#{GEM_DIR}/lib/contracto/server/ruby"
+  CONTRACT_FILENAME     = 'contract.cdc.rb'
   CONTRACT_PID_FILEPATH = "#{CONTRACTO_DIR}/server.pid"
 
   require 'contracto/installer'
