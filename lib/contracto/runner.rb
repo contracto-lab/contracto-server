52c738ff1ef18552060507e268679dd51f73ffa3
Kacper Walanus <kacper@walanus.com>
2015-03-14 13:33:13 +0100

    add TODO

--- lib/contracto/runner.rb
+++ lib/contracto/runner.rb
@@ -6,6 +6,7 @@ class Contracto::Runner
 
   def execute
     system "rackup #{Contracto::CONTRACTO_DIR}/config.ru -p #{PORT} -D -P #{Contracto::CONTRACT_PID_FILEPATH}"
+    # TODO: loop below should terminate after n tries
     system "while ! echo exit | nc localhost #{PORT} > /dev/null && echo \"Waiting for server...\"; do sleep 1; done"
     system "curl 0.0.0.0:#{PORT}"
   end
