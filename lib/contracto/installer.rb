d834acbc19b40fd6a06bdf1dfb7fcc4c47051f86
Kacper Walanus <kacper@walanus.com>
2015-03-14 13:43:38 +0100

    no need for Installer strategy

--- lib/contracto/installer.rb
+++ lib/contracto/installer.rb
@@ -1,13 +1,40 @@
-class Contracto::Installer
-  require_relative 'installer/local'
+require 'fileutils'
 
+class Contracto::Installer
   def initialize(args)
-    if args.empty?
-      @strategy = Contracto::Installer::Local.new
-    end
   end
 
   def execute
-    @strategy.execute
+    remove_old_dir
+    cp_server_files
+    create_contract unless contract_already_exists?
+    puts 'contracto initialized'
+    puts "start writing contracts in #{Contracto::CONTRACT_FILENAME}"
+  rescue
+    revert
+  end
+
+  private
+
+  def remove_old_dir
+    FileUtils.rm_rf Contracto::CONTRACTO_DIR
+  end
+
+  def cp_server_files
+    FileUtils.cp_r Contracto::RUBY_SERVER_DIR, Contracto::CONTRACTO_TMP_DIR
+    FileUtils.mv Contracto::CONTRACTO_TMP_DIR, Contracto::CONTRACTO_DIR
+  end
+
+  def create_contract
+    FileUtils.mv "#{Contracto::CONTRACTO_DIR}/#{Contracto::CONTRACT_FILENAME}", FileUtils.pwd
+    puts "created: #{installer.CONTRACT_FILENAME}"
+  end
+
+  def contract_already_exists?
+    File.exist? Contracto::CONTRACT_FILENAME
+  end
+
+  def revert
+    # TODO: implement
   end
 end
