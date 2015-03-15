3a5a0fd3ab006fdcba822e2a1a90db030d0f8515
Kacper Walanus <kacper@walanus.com>
2015-03-15 14:45:07 +0100

    Contracto version in test request

--- lib/contracto/server/ruby/server.rb
+++ lib/contracto/server/ruby/server.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
+
+get '/contracto' do
+  "*** Contracto server is working! [#{Gem::Specification.find_by_name('contracto').version}] ***"
+end
+
 require './contract.cdc.rb'
 
-get '/' do
-  '*** Contracto server is working! ***'
-end
