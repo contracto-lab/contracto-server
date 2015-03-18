4bad1d947875e6bff26f59e7eb58f30fab42ee81
Kacper Walanus <kacper@walanus.com>
2015-03-18 22:02:50 +0100

    storing server files in hidden directory

--- lib/contracto.rb
+++ lib/contracto.rb
@@ -2,7 +2,7 @@ require_relative 'contracto/version' # TODO: replace all #require with #require_
 
 module Contracto
   GEM_DIR               = Gem::Specification.find_by_name('contracto').gem_dir
-  CONTRACTO_DIR         = 'contracto'
+  CONTRACTO_DIR         = '.contracto'
   CONTRACTO_TMP_DIR     = '.tmp.contracto'
   RUBY_SERVER_DIR       = "#{GEM_DIR}/lib/contracto/server/ruby"
   CONTRACT_FILENAME     = 'contract.con.json'
