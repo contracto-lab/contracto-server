7bcb7b2d025c1b3e83c0b7fb18a5eb80db798a63
Kacper Walanus <kacper@walanus.com>
2015-04-11 00:59:31 +0200

    update start_from_remote script

--- script/start_from_remote.sh
+++ script/start_from_remote.sh
@@ -2,7 +2,7 @@
 rm -rf tmp
 mkdir tmp
 cd tmp
-contracto start https://github.com/kv109/cdc-sample-contract.git
+contracto start git@github.com:kv109/contracto_sample-contract.git
 curl 0.0.0.0:54321/users
 curl 0.0.0.0:54321/users/1
 curl 0.0.0.0:54321/users/2
