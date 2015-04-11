633365b53227af34625f247b7320bd51343a981a
Kacper Walanus <kacper@walanus.com>
2015-04-11 16:07:07 +0200

    #6 all *.con.json files are parsed (not only contract.con.json)

--- lib/contracto/server/ruby/server.rb
+++ lib/contracto/server/ruby/server.rb
@@ -5,10 +5,11 @@ get '/contracto' do
   "*** Contracto server is working! [#{Gem::Specification.find_by_name('contracto').version}] ***"
 end
 
-json_string = File.read Contracto::Constants::CONTRACT_FILENAME
-json_strings = [json_string]
+jsons_with_contracts = Dir["#{Contracto::Constants::ROOT_DIR}/**/*.con.json"].map do |file_with_contract|
+  File.read file_with_contract
+end
 
-Contracto::Parser.new(json_strings).contracts.each do |contract|
+Contracto::Parser.new(jsons_with_contracts).contracts.each do |contract|
   send(contract.http_method, contract.url_pattern) do
     contract.response_body(params)
   end
